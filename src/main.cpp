1a6af71ed4b48764ab3a6187cb646aad6ea9d19e
Jonas Hucklenbroich <hucklenbroichjon@gmail.com>
2019-04-07 22:00:38 +0200

    Protocol optimized and working.

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,11 @@
 #include <LiquidCrystal_I2C.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <EEPROM.h>
 
 boolean startup = true;
+boolean send = false;
+unsigned long previousMillis = 0;
 
 #define ONE_WIRE_BUS 6
 OneWire oneWire(ONE_WIRE_BUS);
@@ -17,8 +20,8 @@ int serialMode = 2;
 const int dht22Pin = 2; // DHT22 daten Pin
 const int heatPin = 4; //Pin zur Heizungssteuerung (An = Heizen, Aus = nicht Heizen)
 
-float tempMin = 23; // Minimale Temperatur (Heizung geht an wenn kleiner)
-float tempMax = 25; // Maximale Temperatur (Heizung geht aus wenn größer)
+float tempMin = 5; // Minimale Temperatur (Heizung geht an wenn kleiner)
+float tempMax = 10; // Maximale Temperatur (Heizung geht aus wenn größer)
 
 float t; // Variable für die Temperatur
 float h; // Variable für die Luftfeuchtigkeit
@@ -34,7 +37,11 @@ void logo(int zeile);
 void temp();
 void serialPrint();
 void sendData();
+void getData(String input);
 void getData();
+void eepromWrite(int start, String string);
+void debugPrint(String input);
+String eepromRead(int start);
 
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7 , 3, POSITIVE);
 DHT dht(dht22Pin,DHT22);
@@ -74,17 +81,20 @@ void setup() {
   lcd.setCursor(0,1); // in zweite Zeile wechseln
   lcd.print("Hochfahren..."); // Hochfahren... schreiben
   dht.begin(); // Temp. und Luftf. Sensor initialisieren
-  delay(1000); // Pause
+  //delay(1000); // Pause
   lcd.setCursor(0,0);
   sensors.begin();
   pinMode(5, INPUT);
+  getData(eepromRead(0));
+  Serial.println("eeprom(0): " + eepromRead(0));
+  delay(2500);
+  Serial.println(String("#") + String("T") + 0.00 + String("H") + 0.00 + String("S") + tempMin + String("X") + tempMax + String(";"));
 }
 
 void loop() {
   getData();
+  sendData();
   if (startup == true) {
-    delay(100);
-    Serial.println("Hi");
     startup = false;
   }
   // Code in loop wird dauernd wiederholt
@@ -94,7 +104,7 @@ void loop() {
     t = dht.readTemperature(); // Temperatur auslesen
     h = dht.readHumidity(); // Luftfeuchtigkeit auslesen
   }else{
-    sensors.requestTemperatures();
+    sensors.requestTemperatures();    // delay(1000)
     t = sensors.getTempCByIndex(0);
     h = 0.0;
   }
@@ -130,28 +140,69 @@ void loop() {
   }
 }
 
-void sendData() {
-  String data;
-  data = String("#") + String("T") + t + String("H") + h + String("S") + tempMin + String("X") + tempMax + String(";");
-  Serial.println(data);
+void eepromWrite(int start, String string) {
+  char charToSave[100];
+  string.toCharArray(charToSave, 100);
+  EEPROM.put(start,charToSave);
+  Serial.println("EEPROM written: " + eepromRead(start));
 }
-void getData() {
-  delay(2000);
-  while (Serial.available()) {
-    String input = Serial.readString();
-    if (input.charAt(0) == 'S' && input.charAt(input.length()-1) == ';'){
-      tempMin = input.substring(input.indexOf("S")+1,input.indexOf("X")-1).toFloat();
-      tempMax = input.substring(input.indexOf("X")+1,input.indexOf(";")-1).toFloat();
-      lcd.clear();
-      lcd.home();
-      lcd.println(tempMin);
-      lcd.print(tempMax);
 
-    }
-    sendData();
+String eepromRead(int start) {
+  char Buffer[100] = {0};
+  EEPROM.get(start, Buffer);
+  return Buffer;
+}
+
+void debugPrint(String input) {
+  lcd.clear();
+  lcd.print(input);
+  Serial.print(input);
+}
+void sendData() {
+  long currentMillis = millis();
+  int interval = 1000;
+  if (send == true && currentMillis - previousMillis >= interval){
+    String data = String("#") + String("T") + t + String("H") + h + String("S") + tempMin + String("X") + tempMax + String(";");
+    Serial.println(data);
+    send = false;
   }
 }
-
+void getData(String input) {
+  float newMin = 0;
+  float newMax = 0;
+  lcd.clear();
+  lcd.print(input);
+  if (input.charAt(0) == 'S' && input.charAt(input.length()-1) == ';'){
+    newMin = input.substring(input.indexOf("S")+1,input.indexOf("X")-1).toFloat();
+    newMax = input.substring(input.indexOf("X")+1,input.indexOf(";")-1).toFloat();
+    tempMin = newMin;
+    tempMax = newMax;
+  }
+  send = true;
+  previousMillis = millis();
+}
+void getData() {
+  while (Serial.available()) {
+    String input = Serial.readString();
+    float newMin = 0;
+    float newMax = 0;
+    lcd.clear();
+    lcd.print(input);
+    if (input.charAt(0) == 'S' && input.charAt(input.length()-1) == ';'){
+      if (millis() > 11000) {
+        newMin = input.substring(input.indexOf("S")+1,input.indexOf("X")-1).toFloat();
+        newMax = input.substring(input.indexOf("X")+1,input.indexOf(";")-1).toFloat();
+      }
+      if (newMin != tempMin || newMax != tempMax) {
+        eepromWrite(0, String("S") + newMin + String("X") + String(newMax) + String(";"));
+      }
+      tempMin = newMin;
+      tempMax = newMax;
+    }
+    send = true;
+    previousMillis = millis();
+  }
+}
 void temp() { // Sensor Werte auf Display anzeigen
   lcd.clear();
   lcd.setCursor(0,0);
